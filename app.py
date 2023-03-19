34c0b2a0519b1b2e221e7a101c2470d81bd1e8d7
Mahesh <srikakulammahesh2085@gmail.com>
2023-03-19 22:20:32 +0530

    test

--- app.py
+++ app.py
@@ -1,83 +1,8 @@
-from flask import Flask,jsonify, render_template, request
-import pandas as pd
-from googletrans import Translator
-
+from flask import Flask, render_template
 
 app = Flask(__name__)
-translator = Translator()
 
-data = {
-    "readable": False,
-    "data":[]
-}
 
 @app.route('/')
-def home():
-    print("home page loaded")
-    return 'working skeleton inside'
-
-@app.route('/push')
-def push():
-    return render_template('home.html')
-
-@app.route('/pullView')
-def pullView():
-    data['readable'] = False
-    return render_template("statement.html",data=data['data'])
-
-@app.route('/pull')
-def pull():
-    try:
-        if not data['readable']:
-            return jsonify({
-            'status':False,
-            'msg':'Nothing is there bro'
-        })
-        
-        return jsonify({
-            'status':True,
-            'data':data
-        })
-        
-    except Exception as err:
-        return jsonify({
-            'status':False,
-            'msg':str(err)
-        })
-
-@app.route('/pushExel',methods=['GET','POST'])
-def pushExel():
-    
-    
-    if request.method == 'POST':
-        
-        try:
-            file = request.files['file']
-            df = pd.read_excel(file,header=13)
-            data["data"] = []
-            for index, row in df.iterrows():
-                
-                date = str(row[1]).split(' ')[0]
-                amount = row[3]
-                name = translator.translate(row[10], src='en', dest = 'te').text
-                type = row[4]
-                if type == 'C':
-                    
-                    data["data"].append({
-                        'date':date,
-                        'name':name,
-                        'amount':amount
-                    })
-                data['readable']=True
-            
-            return jsonify({
-                "status":True,
-                "data":data
-            })
-        except Exception as err:
-            print(err)
-            return jsonify({
-                "status":False,
-                "msg":str(err)
-            })
-    return 'Not gonna work'
+def hello():
+    return 'Hello, world'
